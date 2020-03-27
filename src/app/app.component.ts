9fe6ab11ad8d1b331c2269b23c949d5e5cead506
Allison Reynoso <allison_reynoso@intranet.itresources>
2020-03-27 12:27:12 -0300

    getting started with angular ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { increment, decrement } from './contador/actions';
+interface AppState {
+  count:number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'redux-app';
+  
+  contador$: Observable<number>;
+  constructor(private store: Store<AppState>){
+    
+    this.contador$ = store.pipe(select('count'));
+  }
+  incrementar(){
+    this.store.dispatch(increment());
+  }
+  decrementar(){
+    this.store.dispatch(decrement());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//NGRX
+import {StoreModule} from '@ngrx/store';
+import { counterReducer } from './contador/contador.reducer';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HijoComponent,
+    NietoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({count:counterReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
